3ae0fb2ec83487a13c028b8aefd9d7488d717791
Suhaan Khan <suhaankhan0032@gmail.com>
2026-04-15 17:20:07 +0000

    feat: convert fahrenheit to celsius with float precision

--- mosh_tutorial/main.cpp
+++ mosh_tutorial/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int a=1;
-    int b=2;
-    int box=a;
-    a=b;
-    b=box;
-    std::cout << b << std::endl;
+    cout << "Enter the temperature in Fahrenheit: ";
+    double fahrenheit;
+    cin >> fahrenheit;
+    double celsius = (fahrenheit - 32)*5.0/9.0;
+    cout << "Temperature in Celsius: " << celsius << endl;
     return 0;
 }
