a99367f816b31044836d2428c7b556e7cdd5e1fc
Suhaan Khan <suhaankhan0032@gmail.com>
2026-04-19 16:42:03 +0000

    feat: modularize loop logic with custom header and clean build

---
+++ cherno_tutorials/src/loop.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "loop.h"
+
+int main() {
+    for(int i = 0; i < 4; i++)
+        out("hello world");
+}
---
+++ cherno_tutorials/src/loop.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+
+void out(const char* message) {
+    std::cout << message << std::endl;
+}
